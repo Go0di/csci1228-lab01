197b9a19aa5843a0eaf5cadc8c8662e3abc7cf78
Godluck Simba <godluck.simba@smu.ca>
2026-09-17 12:09:41 -0300

    Record actual results

---
+++ src/Trace2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Meshack Hando
+ */
+public class Trace2 {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 1; i < 5; i++) {
+            total += i;
+        }
+        System.out.println(total);
+
+        int n = 5;
+        while (n > 0) {
+            n = n - 2;
+        }
+        System.out.println(n);
+    } 
+}
---
+++ src/Trace3.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Godluck Simba
+ */
+public class Trace3 {
+    public static void main(String[] args) {
+        int score = 45;
+
+        if (score > 90)
+            if (score > 95)
+                System.out.println("A+");
+        else
+            System.out.println("Not an A");
+
+        System.out.println("done");
+    }
+    
+}
